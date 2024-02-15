c27baae608908a48c9edc8727b04751f2a3c9510
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2024-02-15 15:30:05 +0100

    fix categoryList eample data ant iterate over it for the screen view

--- app/src/main/kotlin/com/platform/openemoji/screens/SearchScreen.kt
+++ app/src/main/kotlin/com/platform/openemoji/screens/SearchScreen.kt
@@ -2,7 +2,6 @@ package com.platform.openemoji.screens
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -15,9 +14,7 @@ import com.platform.openemoji.components.CategoryScrollCarousel
 
 @Composable
 fun CategoryGrid(name: String) {
-    Button(onClick = {}) {
-        Text(name)
-    }
+    Text(name)
 }
 
 @Composable
@@ -27,28 +24,23 @@ fun SearchScreen() {
             Category("All", "all"),
             Category("Smileys & Emotion", "smileys"),
             Category("People & Body", "people"),
-            Category("Animals & Nature", "smileys"),
-            Category("Food & Drink", "smileys"),
-            Category("Travel & Places", "smileys"),
-            Category("Activities", "smileys"),
-            Category("Objects", "smileys"),
-            Category("Symbols", "smileys"),
-            Category("Flags", "smileys"),
+            Category("Animals & Nature", "animals"),
+            Category("Food & Drink", "food"),
+            Category("Travel & Places", "travel"),
+            Category("Activities", "activities"),
+            Category("Objects", "object"),
+            Category("Symbols", "symbols"),
+            Category("Flags", "flags"),
         )
 
     Column(modifier = Modifier.padding(16.dp)) {
         val navController = rememberNavController()
         CategoryScrollCarousel(navController, categoryList)
         NavHost(navController = navController, startDestination = "all") {
-            composable("all") {
-                CategoryGrid(
-                    "all",
-                ) // TODO: Create CategoryGrid which displays emojis for categories.
-            }
-            composable("smileys") {
-                CategoryGrid(
-                    "smileys",
-                ) // TODO: Create CategoryGrid which displays emojis for categories.
+            categoryList.forEach { category ->
+                composable(category.route) {
+                    CategoryGrid(category.name)
+                }
             }
         }
     }
