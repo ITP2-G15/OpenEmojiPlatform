5794fcca2d02ce4ba615d82c582454c2e138b819
Andreas Li <andrea10@stud.ntnu.no>
2024-02-15 11:35:39 +0100

    add EmojiIcon

--- app/src/main/kotlin/com/platform/openemoji/MainActivity.kt
+++ app/src/main/kotlin/com/platform/openemoji/MainActivity.kt
@@ -9,6 +9,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.platform.openemoji.components.CategoryScrollBox
+import com.platform.openemoji.components.EmojiIcon
+
 import com.platform.openemoji.ui.theme.OpenEmojiPlatformTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,6 +24,7 @@ class MainActivity : ComponentActivity() {
                 ) {
                     Column {
                         CategoryScrollBox()
+                        EmojiIcon()
                     }
                 }
             }
