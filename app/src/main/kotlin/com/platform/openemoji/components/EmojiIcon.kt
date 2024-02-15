fb2cb0c618e5c8e87805e4df764f56768f937a0f
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2024-02-15 15:23:19 +0100

    Fix linting error in EmojiIcon

--- app/src/main/kotlin/com/platform/openemoji/components/EmojiIcon.kt
+++ app/src/main/kotlin/com/platform/openemoji/components/EmojiIcon.kt
@@ -9,11 +9,15 @@ import coil.compose.AsyncImage
 
 @Composable
 fun EmojiIcon() {
+    val imageUrl =
+        "https://static-00.iconduck.com/assets.00/" +
+            "smiling-face-with-sunglasses-emoji-512x512-7y1eta1y.png"
     AsyncImage(
-        model = "https://static-00.iconduck.com/assets.00/smiling-face-with-sunglasses-emoji-512x512-7y1eta1y.png",
+        model = imageUrl,
         contentDescription = "emoji.title",
-        modifier = Modifier
-            .size(65.dp)
-            .padding(top = 10.dp)
+        modifier =
+            Modifier
+                .size(65.dp)
+                .padding(top = 10.dp),
     )
 }
