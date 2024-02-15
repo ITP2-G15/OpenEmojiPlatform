29f069d8477c2fe4376572f17d5833e5c27f1e77
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2024-02-15 14:54:06 +0100

    Add CategoryScrollCarousel first draft and navigation for SearchScreen
    Co-authored-by: Steffenriv <Steffenriveim01@gmail.com>

--- app/src/main/kotlin/com/platform/openemoji/screens/SearchScreen.kt
+++ app/src/main/kotlin/com/platform/openemoji/screens/SearchScreen.kt
@@ -2,6 +2,7 @@ package com.platform.openemoji.screens
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
