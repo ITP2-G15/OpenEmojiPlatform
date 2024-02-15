3027ab84562901304095f0de7bb1ba5d5f74bde4
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2024-02-15 14:54:06 +0100

    Add CategoryScrollCarousel first draft and navigation for SearchScreen
    Co-authored-by: Steffenriv <Steffenriveim01@gmail.com>

--- app/src/main/kotlin/com/platform/openemoji/MainActivity.kt
+++ app/src/main/kotlin/com/platform/openemoji/MainActivity.kt
@@ -8,7 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.platform.openemoji.components.CategoryScrollBox
+import com.platform.openemoji.screens.SearchScreen
 import com.platform.openemoji.components.EmojiIcon
 
 import com.platform.openemoji.ui.theme.OpenEmojiPlatformTheme
@@ -23,7 +23,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background,
                 ) {
                     Column {
-                        CategoryScrollBox()
+                        SearchScreen()
                         EmojiIcon()
                     }
                 }
