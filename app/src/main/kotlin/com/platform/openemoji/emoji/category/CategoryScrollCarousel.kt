c1306f479ec4099d8a52a3e0623f7b30a2ce95a7
Younes Bouhmidi <younesb@stud.ntnu.no>
2024-02-22 10:39:00 +0100

    feat: :sparkles: added a search bar at the top of the page. The search functionality does not work yet.

--- app/src/main/kotlin/com/platform/openemoji/emoji/category/CategoryScrollCarousel.kt
+++ app/src/main/kotlin/com/platform/openemoji/emoji/category/CategoryScrollCarousel.kt
@@ -3,7 +3,6 @@ package com.platform.openemoji.emoji.category
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
--- app/src/main/kotlin/com/platform/openemoji/screens/SearchScreen.kt
+++ app/src/main/kotlin/com/platform/openemoji/screens/SearchScreen.kt
@@ -1,9 +1,14 @@
 package com.platform.openemoji.screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
@@ -33,7 +38,22 @@ fun SearchScreen() {
             Category("Flags", "flags"),
         )
 
+    //Creates a state for the search text
+    val searchText = remember { mutableStateOf("") }
+
     Column(modifier = Modifier.padding(16.dp)) {
+
+        //Displays a TextField at the top of the screen
+        TextField(
+            value = searchText.value,
+            onValueChange = { newText -> searchText.value = newText },
+            label = { Text("Search for emoji") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            shape = RoundedCornerShape(12.dp)
+        )
+
         val categoryNav = rememberNavController()
         CategoryScrollCarousel(categoryNav, categoryList)
         NavHost(navController = categoryNav, startDestination = "all") {
