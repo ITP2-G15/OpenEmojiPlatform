e71ae6d83edd6e4f1d71d683595ba3b0608719da
Steffenriv <steffenriveim01@gmail.com>
2024-02-27 16:30:51 +0100

    fixed  ktlint error

--- app/src/main/kotlin/com/platform/openemoji/screens/EmojiDetailScreen.kt
+++ app/src/main/kotlin/com/platform/openemoji/screens/EmojiDetailScreen.kt
@@ -3,7 +3,6 @@ package com.platform.openemoji.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -20,7 +19,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.platform.openemoji.emoji.Emoji
 import com.platform.openemoji.emoji.IconCopy
 
@@ -41,24 +39,24 @@ fun EmojiDetailScreen(emoji: Emoji) {
         }
         Card(
             modifier =
-            Modifier
-                .fillMaxWidth()
-                .size(width = screenWidth, height = 150.dp)
-                .padding(horizontal = 12.dp, vertical = 8.dp),
+                Modifier
+                    .fillMaxWidth()
+                    .size(width = screenWidth, height = 150.dp)
+                    .padding(horizontal = 12.dp, vertical = 8.dp),
         ) {
             Row(
                 modifier =
-                Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 12.dp, vertical = 8.dp),
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 12.dp, vertical = 8.dp),
                 horizontalArrangement = Arrangement.SpaceEvenly,
                 verticalAlignment = Alignment.CenterVertically,
             ) {
                 IconCopy(emoji)
+                // Added padding to hinder Text and IconCopy to collide.
                 Text(
                     text = emoji.title,
-                    modifier = Modifier.padding(12.dp), //So that for large text,
-                    // title will not go to close to emoji
+                    modifier = Modifier.padding(12.dp),
                     style = MaterialTheme.typography.titleLarge,
                     fontWeight = FontWeight.Bold,
                 )
