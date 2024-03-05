2446a40a84bf0fbc1ef07d6d74ba5cc1e6103bd7
Steffenriv <steffenriveim01@gmail.com>
2024-03-05 08:36:42 +0100

    located bugs in navigation using multiple navhost could and will most likely lead to errors, instead we can try to use nested navigations instea, unsure how  to do it so we would have to look into thatd

--- app/src/main/kotlin/com/platform/openemoji/navigation/Navigation.kt
+++ app/src/main/kotlin/com/platform/openemoji/navigation/Navigation.kt
@@ -1,10 +1,6 @@
 package com.platform.openemoji.navigation
 
 import androidx.compose.runtime.Composable
-import
-
-
-import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
@@ -16,61 +12,32 @@ import com.platform.openemoji.emoji.category.route
 import com.platform.openemoji.emoji.category.CategoryScrollCarousel
 import com.platform.openemoji.emoji.catalogue.EmojiCatalogueUi
 import com.platform.openemoji.emoji.catalogue.EmojiCatalogue
-import com.platform.openemoji.navigation.Screen
+import com.platform.openemoji.navigation.NavigationItem
 import com.platform.openemoji.screens.EmojiDetailScreen
 import com.platform.openemoji.screens.SearchScreen
 
 @Composable
 fun Navigation() {
     val emojiCatalogue = EmojiCatalogue.get()
+    val allEmojis = emojiCatalogue.byCategory
 
     val navController = rememberNavController()
 
     NavHost(
         navController = navController,
-        startDestination = Screen.SearchScreen.route,
+        startDestination = NavigationItem.SearchScreen.route,
     ) {
         /**
          * Routing for different screens
          */
-        composable(route = Screen.SearchScreen.route,) {
+        composable(route = NavigationItem.SearchScreen.route,) {
             SearchScreen()
         }
-        composable(
-            route = Screen.EmojiDetailScreen.route
-        ) {backStackEntry ->
-            val emojiName = backStackEntry.arguments?.get("emojiName", Emoji)
-            emojiCatalogue.
-            if (emojiName != null) {
-                //val emoji
-                //if (emoji != null) {
-                //    EmojiDetailScreen(navController, emoji)
-                //}
-            }
-        }
 
-        /**
-         * Routing for categories within SearchScreen.
-         */
-        composable(
-            Screen.AllCategories.route,
-        ) {
-            EmojiCatalogueUi(
-                emojis = emojiCatalogue.byCategory,
-                maxEmojisPerGrid = 15,
-                filterText = ""            )
-        }
-        // Subcategories (e.g. Activities > Sport)
-        emojiCatalogue.categories.forEach { category ->
-            composable(
-                "search/${route(category)}",
-            ) {
-                emojiCatalogue.bySubCategory(category)?.let {
-                    EmojiCatalogueUi(
-                        emojis = it,
-                        maxEmojisPerGrid = 15,
-                        filterText = ""
-                    )
+        allEmojis.forEach { (category, emojis) ->
+            emojis.forEach {emoji ->
+                composable("emoji/${emoji.title}") {
+                    EmojiDetailScreen(emoji = emoji)
                 }
             }
         }
