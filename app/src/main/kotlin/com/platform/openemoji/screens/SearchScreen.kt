b13a89e0503634ecb2145ddf6a84127a339b8464
taleeikenes <talene@stud.ntnu.no>
2024-03-05 15:16:16 +0100

    Add file structure

--- app/src/main/kotlin/com/platform/openemoji/screens/SearchScreen.kt
+++ app/src/main/kotlin/com/platform/openemoji/screens/SearchScreen.kt
@@ -22,12 +22,15 @@ import com.platform.openemoji.emoji.catalogue.EmojiCatalogueUi
 import com.platform.openemoji.emoji.catalogue.grid.EmojiGrid
 import com.platform.openemoji.emoji.category.CategoryScrollCarousel
 import com.platform.openemoji.emoji.category.route
+import com.platform.openemoji.headerlogo.HeaderLogo
 
 @Composable
 fun SearchScreen() {
     val emojiCatalogue = EmojiCatalogue.get()
     // Creates a state for the search text
     val query = remember { mutableStateOf("") }
+    val HeaderLogo = HeaderLogo()
+
 
     Column {
         // Displays a TextField at the top of the screen
