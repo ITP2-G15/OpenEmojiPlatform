efce1c205af23bbd416b223212d55d69ae612a86
oarorvik <oarorvik@stud.ntnu.no>
2024-03-10 11:12:05 +0100

    add most popular section to home screen

--- app/src/main/kotlin/com/platform/openemoji/screens/HomeScreen.kt
+++ app/src/main/kotlin/com/platform/openemoji/screens/HomeScreen.kt
@@ -1,10 +1,29 @@
 package com.platform.openemoji.screens
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
+import com.platform.openemoji.R
+import com.platform.openemoji.emoji.catalogue.EmojiCatalogue
+import com.platform.openemoji.emoji.catalogue.EmojiCatalogueUi
 
 @Composable
 fun HomeScreen(navController: NavController) {
-    Text(text = "Home")
+    Column(
+        modifier = Modifier.testTag("homeScreen"),
+    ) {
+        // Most Popular
+        // Made using EmojiCatalogueUI with only one category: "Most Popular"
+        EmojiCatalogueUi(
+            emojis =
+                mapOf(
+                    stringResource(R.string.most_popular) to
+                        EmojiCatalogue.get().mostPopularEmojis(14),
+                ),
+            navController = navController,
+        )
+    }
 }
