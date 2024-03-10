be7e71334ffaf9dc8753373e00c75b243dc7db2f
oarorvik <oarorvik@stud.ntnu.no>
2024-03-10 10:37:51 +0100

    test navbar screen navigation

--- app/src/main/kotlin/com/platform/openemoji/screens/HomeScreen.kt
+++ app/src/main/kotlin/com/platform/openemoji/screens/HomeScreen.kt
@@ -1,10 +1,17 @@
 package com.platform.openemoji.screens
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.navigation.NavController
 
 @Composable
 fun HomeScreen(navController: NavController) {
-    Text(text = "Home")
+    Column(
+        modifier = Modifier.testTag("homeScreen"),
+    ) {
+        Text(text = "Home")
+    }
 }
