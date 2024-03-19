d191659bf86cd1d5a63805f59b29cd34d44c611b
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2024-03-19 11:03:42 +0100

    fix rename application & activity

--- app/src/main/kotlin/com/platform/openemoji/Activity.kt
+++ app/src/main/kotlin/com/platform/openemoji/Activity.kt
@@ -6,7 +6,7 @@ import androidx.activity.compose.setContent
 import com.platform.openemoji.navigation.Navigation
 import com.platform.openemoji.theme.OpenEmojiPlatformTheme
 
-class EmojiActivity : ComponentActivity() {
+class Activity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
--- app/src/main/kotlin/com/platform/openemoji/Application.kt
+++ app/src/main/kotlin/com/platform/openemoji/Application.kt
@@ -2,7 +2,7 @@ import android.app.Application
 import com.platform.openemoji.emoji.EmojiRepository
 import com.platform.openemoji.news.NewsRepository
 
-class EmojiApplication : Application() {
+class Application : Application() {
     val emojiRepository by lazy { EmojiRepository(this) }
     val newsRepository by lazy { NewsRepository(this) }
 }
