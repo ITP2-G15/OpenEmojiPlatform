d520823e05cb06240a62d2288bfbc7f80161c940
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2024-03-19 13:30:12 +0100

    add EventsRepository to Application

--- app/src/main/kotlin/com/platform/openemoji/Application.kt
+++ app/src/main/kotlin/com/platform/openemoji/Application.kt
@@ -1,8 +1,10 @@
 import android.app.Application
 import com.platform.openemoji.emoji.EmojiRepository
+import com.platform.openemoji.events.EventsRepository
 import com.platform.openemoji.news.NewsRepository
 
 class Application : Application() {
     val emojiRepository by lazy { EmojiRepository(this) }
     val newsRepository by lazy { NewsRepository(this) }
+    val eventsRepository by lazy { EventsRepository(this) }
 }
