483a99de9d474203d16b185face9fb82cc83e1e1
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2024-03-19 13:29:22 +0100

    add events mockdata and EventRepository

--- app/src/main/kotlin/com/platform/openemoji/events/EventsRepository.kt
+++ app/src/main/kotlin/com/platform/openemoji/events/EventsRepository.kt
@@ -1,4 +1,4 @@
-package com.platform.openemoji.news
+package com.platform.openemoji.events
 
 import android.content.Context
 import kotlinx.coroutines.Dispatchers
@@ -7,31 +7,31 @@ import kotlinx.coroutines.withContext
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 
-class NewsRepository(private val context: Context) {
+class EventsRepository(private val context: Context) {
     // This is only needed when not using an API
-    private var mockdata: List<News>? = null
+    private var mockdata: List<Event>? = null
 
     // This method loads the emojis from the assets folder and is only needed when not using an API
-    private suspend fun loadMockdata(simulatedDelay: Long = 0): List<News> {
+    private suspend fun loadMockdata(simulatedDelay: Long = 0): List<Event> {
         // Simulate a delay to show the loading state
         delay(simulatedDelay)
 
         if (mockdata != null) return mockdata!!
 
         withContext(Dispatchers.IO) {
-            val inputStream = context.assets.open("news.json")
+            val inputStream = context.assets.open("events.json")
             val size = inputStream.available()
             val buffer = ByteArray(size)
             inputStream.read(buffer)
             inputStream.close()
             val json = String(buffer, Charsets.UTF_8)
-            mockdata = Json.decodeFromString<List<News>>(json)
+            mockdata = Json.decodeFromString<List<Event>>(json)
         }
 
         return mockdata!!
     }
 
-    suspend fun getNews(limit: Int = Int.MAX_VALUE): List<News> {
+    suspend fun getEvents(limit: Int = Int.MAX_VALUE): List<Event> {
         val allNews = loadMockdata(1000)
         return allNews.take(limit)
     }
