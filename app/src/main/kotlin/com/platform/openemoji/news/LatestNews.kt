a790467120c85c00070015ccaf614894e0329ba7
Andreas Li <andrea10@stud.ntnu.no>
2024-03-19 20:40:48 +0100

    add NewsCard

--- app/src/main/kotlin/com/platform/openemoji/news/LatestNews.kt
+++ app/src/main/kotlin/com/platform/openemoji/news/LatestNews.kt
@@ -1,10 +1,17 @@
 package com.platform.openemoji.news
 
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 
 @Composable
 fun LatestNews(navController: NavController) {
-    Text(text = "testing man")
+    val news =
+        News(
+            "First Look: New Emojis in iOS 17.4",
+            "https://blog.emojipedia.org/first-look-new-emojis-in-ios-17-4/",
+            "https://blog.emojipedia.org/content/images/size/w2000/2024/01/Emojipedia-iOS-Apple-Emoji-15_0-Header.jpg",
+            "New emojis have arrived on iOS as part of the first iOS 17.4 beta. The new additions include a phoenix, a lime, smileys shaking their heads up and down, and a series of direction-specifying people emojis.",
+        )
+
+    NewsCard(news)
 }
--- app/src/main/kotlin/com/platform/openemoji/news/NewsCard.kt
+++ app/src/main/kotlin/com/platform/openemoji/news/NewsCard.kt
@@ -1,9 +1,87 @@
 package com.platform.openemoji.news
 
+import android.content.Intent
+import android.net.Uri
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 
 @Composable
 fun NewsCard(news: News) {
-    Text(text = "testing")
+    val screenWidth = LocalConfiguration.current.screenWidthDp.dp
+    val context = LocalContext.current
+    val eventIntent = remember { Intent(Intent.ACTION_VIEW, Uri.parse(news.url)) }
+    val imagePainter = rememberImagePainter(news.img)
+
+    Card(
+        modifier =
+            Modifier
+                .fillMaxWidth()
+                .size(width = screenWidth, height = 200.dp)
+                .padding(horizontal = 12.dp, vertical = 8.dp)
+                .clickable {
+                    context.startActivity(eventIntent)
+                },
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+        ) {
+            Box(modifier = Modifier.fillMaxSize()) {
+                Image(
+                    painter = imagePainter,
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxSize(),
+                    alignment = Alignment.Center,
+                    contentScale = ContentScale.Crop,
+                )
+                Card(
+                    modifier =
+                        Modifier
+                            .size(width = screenWidth, height = 80.dp)
+                            .align(Alignment.BottomStart),
+                    shape = RoundedCornerShape(2.dp),
+                    colors =
+                        CardDefaults.cardColors(
+                            containerColor = Color.LightGray.copy(alpha = 0.8f),
+                            contentColor = MaterialTheme.colorScheme.onBackground,
+                        ),
+                ) {
+                    Text(
+                        text = news.title,
+                        modifier = Modifier.padding(horizontal = 12.dp, vertical = 4.dp),
+                        style = MaterialTheme.typography.titleLarge,
+                        fontWeight = FontWeight.Medium,
+                    )
+                    Text(
+                        text = news.desc,
+                        modifier = Modifier.padding(horizontal = 12.dp),
+                    )
+                }
+            }
+        }
+    }
 }
