ee00999de844f2f4f11533a15b298f2f58c55f49
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2024-03-19 11:01:11 +0100

    add news repository to application

--- app/src/main/kotlin/com/platform/openemoji/EmojiApplication.kt
+++ app/src/main/kotlin/com/platform/openemoji/EmojiApplication.kt
@@ -1,6 +1,8 @@
 import android.app.Application
 import com.platform.openemoji.emoji.EmojiRepository
+import com.platform.openemoji.news.NewsRepository
 
 class EmojiApplication : Application() {
     val emojiRepository by lazy { EmojiRepository(this) }
+    val newsRepository by lazy { NewsRepository(this) }
 }
