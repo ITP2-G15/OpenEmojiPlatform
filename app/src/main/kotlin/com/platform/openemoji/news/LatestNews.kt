00a9399047c229d17ba974294f130c99d4b2b9fa
Andreas Li <andrea10@stud.ntnu.no>
2024-03-25 12:53:35 +0100

    fix max 90 line error

--- app/src/main/kotlin/com/platform/openemoji/news/LatestNews.kt
+++ app/src/main/kotlin/com/platform/openemoji/news/LatestNews.kt
@@ -26,14 +26,23 @@ fun LatestNews(navController: NavController) {
             News(
                 "First Look: New Emojis in iOS 17.4",
                 "https://blog.emojipedia.org/first-look-new-emojis-in-ios-17-4/",
-                "https://blog.emojipedia.org/content/images/size/w2000/2024/01/Emojipedia-iOS-Apple-Emoji-15_0-Header.jpg",
-                "New emojis have arrived on iOS as part of the first iOS 17.4 beta. The new additions include a phoenix, a lime, smileys shaking their heads up and down, and a series of direction-specifying people emojis.",
+                "https://blog.emojipedia.org/content/images/size/" +
+                    "w2000/2024/01/Emojipedia-iOS-Apple-Emoji-15_0-Header.jpg",
+                "New emojis have arrived on iOS as part of the first iOS 17.4 beta." +
+                    " The new additions include a phoenix, a lime, smileys shaking" +
+                    " their heads up and down, and a series of direction-specifying" +
+                    " people emojis.",
             ),
             News(
                 "Google's Emoji 15.1 Support In Noto Color Emoji",
-                "https://blog.emojipedia.org/googles-emoji-15-1-support-in-noto-color-emoji/",
-                "https://blog.emojipedia.org/content/images/size/w2000/2023/11/Emojipedia-Google-Emoji-15_1-Header.jpg",
-                "Today Google has officially unveiled its full-color designs for Unicode's latest approved emojis, which include a phoenix, a lime, smileys shaking their heads up and down, and a series of direction-specifying people emojis.",
+                "https://blog.emojipedia.org/googles-emoji" +
+                    "-15-1-support-in-noto-color-emoji/",
+                "https://blog.emojipedia.org/content/images/size/w2000/" +
+                    "2023/11/Emojipedia-Google-Emoji-15_1-Header.jpg",
+                "Today Google has officially unveiled its full-color des" +
+                    "igns for Unicode's latest approved emojis, which include" +
+                    " a phoenix, a lime, smileys shaking their heads up and down, " +
+                    "and a series of direction-specifying people emojis.",
             ),
         )
 
