628df5161411db616393eddaf90ff876907dd1d1
oarorvik <oarorvik@stud.ntnu.no>
2024-03-31 14:25:52 +0200

    replace search field test with a simpler one
    We should test the search screen later

--- app/src/androidTest/kotlin/com/platform/openemoji/search/SearchFieldTest.kt
+++ app/src/androidTest/kotlin/com/platform/openemoji/search/SearchFieldTest.kt
@@ -1,49 +1,32 @@
 package com.platform.openemoji.search
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTextInput
+import org.junit.Rule
+import org.junit.Test
+
 class SearchFieldTest {
-    /*
     @get:Rule
     val composeTestRule = createComposeRule()
 
-    private val emojiCatalogue = SearchScreenEmojiCatalogue.get()
-
-    @Before
-    fun init() {
-        emojiCatalogue.populate(
-            listOf(
-                Emoji("1", "aaa", "\uD83D\uDE00", "A", "AA", "", ""),
-                Emoji("2", "abc", "\uD83D\uDE02", "A", "AA", "", ""),
-                Emoji("3", "bbb", "\uD83D\uDC4D", "A", "AB", "", ""),
-                Emoji("4", "ccc", "\uD83D\uDD25", "B", "BA", "", ""),
-            ),
-        )
-    }
-
     @Test
-    fun testSearch() {
-        val navController =
-            TestNavHostController(ApplicationProvider.getApplicationContext())
-
+    fun testSearchField() {
         composeTestRule.setContent {
-            SearchScreen(navController)
+            val searchQuery =
+                remember {
+                    mutableStateOf("")
+                }
+            SearchField(searchQuery.value) { searchQuery.value = it }
         }
 
-        // Test that there are 4 emoji icons on the screen before search
-        composeTestRule.onAllNodesWithTag("iconRoute").assertCountEquals(4)
+        composeTestRule.onNodeWithTag("searchTextField")
+            .performTextInput("hello")
 
-        // Perform search
-        val query = "A"
-        composeTestRule.onNodeWithTag("searchTextField").performTextInput(query)
-
-        // test that search header exists
-        composeTestRule.onNodeWithText("Search results for: $query").assertExists()
-
-        // Test that there are 2 emoji icons on the screen
-        composeTestRule.onAllNodesWithTag("iconRoute").assertCountEquals(2)
-
-        // Test that there are 4 emoji icons on the screen after clearing input
-        composeTestRule.onNodeWithTag("searchTextField").performTextClearance()
-        composeTestRule.onAllNodesWithTag("iconRoute").assertCountEquals(4)
+        composeTestRule.onNodeWithText("hello").assertExists()
     }
-     */
 }
