8ea7c87103df7c12350ff2d11f967c50923ac881
Steffrei <steffrei@stud.ntnu.no>
2024-04-04 15:33:07 +0200

    added back fillmaxsize on surface in navigation

--- app/src/main/kotlin/com/platform/openemoji/navigation/Navigation.kt
+++ app/src/main/kotlin/com/platform/openemoji/navigation/Navigation.kt
@@ -65,7 +65,7 @@ fun Navigation(
         bottomBar = { BottomNavigationBar(navController, startDestination) },
     ) { paddingValues ->
         Surface(
-            modifier = Modifier.padding(paddingValues),
+            modifier = Modifier.padding(paddingValues).fillMaxSize(),
             color = MaterialTheme.colorScheme.background,
         ) {
             NavHost(
