ad06f3c92009575cf819acba9f81874d399edfd1
Younes Bouhmidi <younesb@stud.ntnu.no>
2024-04-04 16:08:02 +0200

    feat: :sparkles: Made it possible to display sequence of emojis and copy them at favorite screen

---
+++ app/src/main/kotlin/com/platform/openemoji/favorites/Sequence.kt
@@ -0,0 +1,96 @@
+package com.platform.openemoji.favorites
+
+import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ContentCopy
+import androidx.compose.material.icons.filled.DeleteOutline
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.platform.openemoji.R
+
+@Composable
+fun Sequence(favorite: Favorite) {
+    val clipboardManager = LocalClipboardManager.current
+    val context = LocalContext.current
+
+    Card(
+        modifier =
+            Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 12.dp, vertical = 8.dp),
+    ) {
+        Column(
+            modifier =
+                Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text(
+                text = favorite.name,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(bottom = 8.dp),
+            )
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalAlignment = Alignment.Start,
+            ) {
+                val emojiRegex = "[\\uD83C-\\uDBFF\\uDC00-\\uDFFF]+".toRegex()
+                val emojis =
+                    emojiRegex.findAll(
+                        favorite.emojiSequence,
+                    ).map { it.value }.toList()
+                emojis.chunked(5).forEach { chunk ->
+                    Text(
+                        text = chunk.joinToString(""),
+                        fontSize = 22.sp,
+                        modifier = Modifier.padding(bottom = 8.dp),
+                    )
+                }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                ) {
+                    Button(onClick = {
+                        clipboardManager.setText(AnnotatedString(favorite.emojiSequence))
+                        Toast.makeText(
+                            context,
+                            "Emoji sequence copied to clipboard",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                    }) {
+                        Icon(Icons.Filled.ContentCopy, contentDescription = "Copy Icon")
+                        Text(text = stringResource(R.string.copy_sequence))
+                    }
+                    Button(onClick = {
+                        // HANDLE DELETE HERE
+                    }) {
+                        Icon(
+                            Icons.Filled.DeleteOutline,
+                            contentDescription = "Delete Icon",
+                        )
+                        Text(text = context.getString(R.string.delete_sequence))
+                    }
+                }
+            }
+        }
+    }
+}
