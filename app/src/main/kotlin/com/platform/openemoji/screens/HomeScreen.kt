676a6cd3b49541d6ea5b80a65dd83a5e03536589
aurorakennedy <aurorke@stud.ntnu.no>
2024-04-05 16:05:33 +0200

    structur setup

--- app/src/main/kotlin/com/platform/openemoji/screens/HomeScreen.kt
+++ app/src/main/kotlin/com/platform/openemoji/screens/HomeScreen.kt
@@ -19,6 +19,7 @@ import com.platform.openemoji.emoji.catalogue.EmojiCatalogue
 import com.platform.openemoji.emoji.catalogue.EmojiCatalogueViewModel
 import com.platform.openemoji.events.EventViewModel
 import com.platform.openemoji.events.UpcomingEvents
+import com.platform.openemoji.game.GameScreen
 import com.platform.openemoji.header.HeaderLogo
 import com.platform.openemoji.news.LatestNews
 import com.platform.openemoji.news.NewsViewModel
@@ -58,5 +59,7 @@ fun HomeScreen(
         InlineAd()
 
         LatestNews(newsViewModel, navController)
+
+        GameScreen()
     }
 }
