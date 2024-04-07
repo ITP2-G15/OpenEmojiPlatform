99909b4b2adc973637d815e09793269f33159797
Andreas Li <andrea10@stud.ntnu.no>
2024-04-07 14:55:10 +0200

    add viewmodel

--- app/src/main/kotlin/com/platform/openemoji/game/GameViewModel.kt
+++ app/src/main/kotlin/com/platform/openemoji/game/GameViewModel.kt
@@ -1,3 +1,43 @@
 package com.platform.openemoji.game
 
-class GameViewModel
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class GameViewModel(
+    private val gameRepository: GameRepository,
+) : ViewModel() {
+    // not sure if this is ever needed?
+    private val _allLevels = MutableStateFlow<List<Level>>(emptyList())
+    val allLevels = _allLevels.asStateFlow()
+
+    private val _currentLevel = MutableStateFlow<Level?>(null)
+    val currentLevel = _currentLevel.asStateFlow()
+
+    private val _levelCounter = MutableStateFlow(0)
+    val levelCounter = _levelCounter.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            _allLevels.value = gameRepository.getAllLevels()
+        }
+
+        viewModelScope.launch {
+            gameRepository.getLevelCounter().collect { counter ->
+                _levelCounter.value = counter
+            }
+        }
+
+        viewModelScope.launch {
+            _currentLevel.value = gameRepository.getCurrentLevel()
+        }
+    }
+
+    fun incrementLevelCounter() {
+        viewModelScope.launch {
+            gameRepository.incrementLevelCounter()
+        }
+    }
+}
