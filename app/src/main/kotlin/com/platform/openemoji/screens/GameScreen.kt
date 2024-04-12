bfb76e083222da1773dfe8a618a86907e798e254
Tale Nygaard  Eikenes <taleeikenes@Tales-MBP.localdomain>
2024-04-12 18:36:17 +0200

    Add logic to nav item Games

--- app/src/main/kotlin/com/platform/openemoji/screens/GameScreen.kt
+++ app/src/main/kotlin/com/platform/openemoji/screens/GameScreen.kt
@@ -1,15 +1,11 @@
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
 
-@OptIn(ExperimentalMaterial3Api::class)
+// @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun GameScreen() {
+fun GameScreen(navController: NavController? = null) {
    /* Scaffold(
         topBar = {
             TopAppBar(
@@ -22,7 +18,6 @@ fun GameScreen() {
         )
     }*/
     Text("This is the Game Screen")
-
 }
 
 @Preview(showBackground = true)
