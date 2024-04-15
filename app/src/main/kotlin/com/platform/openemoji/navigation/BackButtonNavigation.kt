93f73d0ea3897b779af9540f1f74570790ba86fa
Steffenriv <steffenriveim01@gmail.com>
2024-04-15 12:04:14 +0200

    Fixed navigation bug when spamming return button Added a navigationInProgress state which checks if current composable has been clicked and is currently navigating. This should fix the issue and thus it will no longer be possible to spam return button and pop backstack multiple times.

--- app/src/main/kotlin/com/platform/openemoji/navigation/BackButtonNavigation.kt
+++ app/src/main/kotlin/com/platform/openemoji/navigation/BackButtonNavigation.kt
@@ -8,6 +8,10 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
@@ -21,9 +25,19 @@ fun BackButtonNavigation(
     modifier: Modifier = Modifier,
     onClick: () -> Unit = {},
 ) {
+    // Track if a navigation is in progress (to prevent multiple pops of backstack)
+    // NOTE: Since this is a composable, it will reset on page reloads and when
+    // navigation is complete.
+    var navigationInProgress by rememberSaveable { mutableStateOf(false) }
+
     IconButton(onClick = {
-        navController.popBackStack()
-        onClick()
+        // Check if a navigation is already in progress
+        if (!navigationInProgress) {
+            // If not, pop the back stack and set navigationInProgress to true
+            navController.popBackStack()
+            navigationInProgress = true
+            onClick()
+        }
     }) {
         Icon(
             Icons.AutoMirrored.Default.ArrowBack,
