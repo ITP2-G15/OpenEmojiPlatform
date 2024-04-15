17c4f430af1494ee0a302772ee669cffde9cf0d6
Tale Nygaard  Eikenes <taleeikenes@dhcp-10-24-71-129.wlan.ntnu.no>
2024-04-15 14:58:19 +0200

    Change to TopBottomAd instead of InlineAd

--- app/src/main/kotlin/com/platform/openemoji/screens/EmojiDetailScreen.kt
+++ app/src/main/kotlin/com/platform/openemoji/screens/EmojiDetailScreen.kt
@@ -26,7 +26,7 @@ import androidx.core.text.HtmlCompat
 import androidx.navigation.NavController
 import com.google.android.gms.ads.interstitial.InterstitialAd
 import com.platform.openemoji.ads.AdSettings
-import com.platform.openemoji.ads.InlineAd
+import com.platform.openemoji.ads.TopBottomAd
 import com.platform.openemoji.ads.loadInterstitialAd
 import com.platform.openemoji.emoji.Emoji
 import com.platform.openemoji.emoji.IconCopy
@@ -67,7 +67,7 @@ fun EmojiDetailScreen(
             interstitialAd.value?.show(context as Activity)
         }
 
-        InlineAd()
+        TopBottomAd()
         Card(
             modifier =
                 Modifier
