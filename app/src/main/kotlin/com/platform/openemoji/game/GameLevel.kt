cd5987e90838fc3543a9a9796f1218a6274b9fc5
aurorakennedy <aurorke@stud.ntnu.no>
2024-04-18 14:34:35 +0200

    Level question showing

--- app/src/main/kotlin/com/platform/openemoji/game/GameLevel.kt
+++ app/src/main/kotlin/com/platform/openemoji/game/GameLevel.kt
@@ -1,37 +1,34 @@
 package com.platform.openemoji.game
 
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun GameLevel(currentLevel: Level) {
-    Column {
-        Row(
-            modifier =
-                Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 12.dp, vertical = 8.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically,
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Box(
+            modifier = Modifier.padding(top = 50.dp),
         ) {
             Text(
                 text = currentLevel.emojiQuestion,
-                style = MaterialTheme.typography.titleLarge,
+                style =
+                    TextStyle(
+                        fontSize = 80.sp,
+                    ),
+                modifier = Modifier.align(Alignment.Center),
             )
         }
-           /* LevelCard(level = level) { isCorrect ->
-                if (isCorrect) {
-                    gameViewModel.incrementLevelCounter()
-                }
-            }*/
     }
 }
