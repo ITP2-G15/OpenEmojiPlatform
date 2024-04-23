296ac1000edbe6e7e4717570e9a81be21fa57514
aurorakennedy <aurorke@stud.ntnu.no>
2024-04-23 11:27:25 +0200

    Styling + header logo

--- app/src/main/kotlin/com/platform/openemoji/game/GameLevel.kt
+++ app/src/main/kotlin/com/platform/openemoji/game/GameLevel.kt
@@ -2,11 +2,13 @@ package com.platform.openemoji.game
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
@@ -18,12 +20,13 @@ fun GameLevel(currentLevel: Level) {
             text = currentLevel.emojiQuestion,
             style =
                 TextStyle(
-                    fontSize = 80.sp,
+                    fontSize = 70.sp,
                 ),
             modifier =
                 Modifier.align(
                     Alignment.TopCenter,
-                ),
+                )
+                    .padding(20.dp),
         )
     }
 }
--- app/src/main/kotlin/com/platform/openemoji/screens/GameScreen.kt
+++ app/src/main/kotlin/com/platform/openemoji/screens/GameScreen.kt
@@ -1,25 +1,35 @@
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.platform.openemoji.game.GameLevel
 import com.platform.openemoji.game.GameViewModel
+import com.platform.openemoji.header.HeaderLogo
 
 @Composable
 fun GameScreen(
@@ -29,17 +39,20 @@ fun GameScreen(
     val currentLevel by gameViewModel.currentLevel.collectAsState()
     val levelCounter by gameViewModel.levelCounter.collectAsState()
 
-    // var answeredCorrectly by remember { mutableStateOf(false) }
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
 
-    // val screenWidth = LocalConfiguration.current.screenWidthDp.dp
-    val cardHeight = 400.dp
-    // val wrongAnswerColor = MaterialTheme.colorScheme.error
+    val cardHeight = 300.dp
+
+    HeaderLogo()
 
     Column(
         modifier =
             Modifier
                 .fillMaxSize()
                 .padding(horizontal = 12.dp, vertical = 8.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         Card(
             modifier =
@@ -47,21 +60,25 @@ fun GameScreen(
                     .fillMaxWidth()
                     .height(cardHeight)
                     .padding(bottom = 16.dp),
+            shape = RoundedCornerShape(16.dp),
         ) {
             Column(
                 modifier = Modifier.padding(16.dp),
                 horizontalAlignment = Alignment.CenterHorizontally,
             ) {
                 Text(
-                    text = "Level $levelCounter",
-                    style = TextStyle(fontSize = 70.sp),
+                    text = "Level $levelCounter \n",
+                    style = TextStyle(fontSize = 40.sp),
+                    fontWeight = FontWeight.Bold,
                 )
                 currentLevel?.let { GameLevel(it) }
             }
         }
+        Spacer(modifier = Modifier.height(16.dp))
 
         Box(
-            modifier = Modifier.padding(horizontal = 8.dp),
+            modifier =
+                Modifier.padding(horizontal = 8.dp),
         ) {
             Column(
                 modifier = Modifier.padding(16.dp),
@@ -69,22 +86,43 @@ fun GameScreen(
                 verticalArrangement = Arrangement.Center,
             ) {
                 currentLevel?.alternatives?.forEachIndexed { index, alternative ->
+                    var isAnswerCorrect by remember { mutableStateOf(true) }
 
                     Button(
                         modifier =
-                            Modifier.padding(
-                                vertical = 10.dp,
-                            ),
+                            Modifier
+                                .padding(vertical = 10.dp)
+                                .fillMaxWidth(1f),
+                        colors =
+                            if (!isAnswerCorrect && isPressed
+                            ) {
+                                ButtonDefaults.buttonColors(
+                                    MaterialTheme.colorScheme.error,
+                                )
+                            } else {
+                                ButtonDefaults.buttonColors(
+                                    MaterialTheme.colorScheme.primary,
+                                )
+                            },
                         onClick = {
                             if (currentLevel?.correctAlternative == index) {
                                 println("Riktig")
                                 gameViewModel.incrementLevelCounter()
+                                isAnswerCorrect = true
                             } else {
                                 println("Feil")
+                                isAnswerCorrect = false
                             }
                         },
+                        interactionSource = interactionSource,
                     ) {
-                        Text(text = alternative)
+                        Text(
+                            text = alternative,
+                            style =
+                                TextStyle(
+                                    fontSize = 25.sp,
+                                ),
+                        )
                     }
                 }
             }
