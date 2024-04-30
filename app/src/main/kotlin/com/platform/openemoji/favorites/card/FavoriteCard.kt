832447c9bf7781fcf3f6b4809628ad78388fd425
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2024-04-30 10:56:45 +0200

    remove unused context

--- app/src/main/kotlin/com/platform/openemoji/favorites/card/FavoriteCard.kt
+++ app/src/main/kotlin/com/platform/openemoji/favorites/card/FavoriteCard.kt
@@ -25,7 +25,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalClipboardManager
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.AnnotatedString
@@ -46,8 +45,6 @@ fun FavoriteCard(
 ) {
     val clipboardManager = LocalClipboardManager.current
     val showDeleteDialog = remember { mutableStateOf(false) }
-
-    val context = LocalContext.current
 
     Card(
         modifier =
