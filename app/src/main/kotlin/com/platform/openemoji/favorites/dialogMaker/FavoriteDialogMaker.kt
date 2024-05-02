2d61131da6c84741f613f9718c4eaa59f1f66b6e
Michael Brusegard <56915010+michaelbrusegard@users.noreply.github.com>
2024-05-02 10:35:13 +0200

    finish new dialog maker

--- app/src/main/kotlin/com/platform/openemoji/favorites/dialogMaker/FavoriteDialogMaker.kt
+++ app/src/main/kotlin/com/platform/openemoji/favorites/dialogMaker/FavoriteDialogMaker.kt
@@ -1,5 +1,6 @@
 package com.platform.openemoji.favorites.dialogMaker
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,6 +13,8 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.Backspace
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.EditNote
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -24,16 +27,26 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import com.platform.openemoji.R
 import com.platform.openemoji.emoji.IconFavorite
 import com.platform.openemoji.favorites.FavoritesViewModel
+import com.platform.openemoji.favorites.maker.FavoriteMakerTextInput
+import com.platform.openemoji.favorites.maker.dialogs.FavoriteSaveDialog
 import com.platform.openemoji.search.SearchField
 import com.platform.openemoji.search.SearchViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @Composable
 fun FavoriteDialogMaker(
@@ -41,6 +54,9 @@ fun FavoriteDialogMaker(
     searchViewModel: SearchViewModel,
     showCreateDialog: MutableState<Boolean>,
 ) {
+    val context = LocalContext.current
+    val focusManager = LocalFocusManager.current
+
     val currentFavorite by favoritesViewModel.currentFavorite.collectAsState()
     val localCurrentFavorite = currentFavorite
 
@@ -50,6 +66,32 @@ fun FavoriteDialogMaker(
 
     val searchResultsAreLoading by searchViewModel.searchResultsAreLoading
         .collectAsState()
+
+    val showSaveDialog = remember { mutableStateOf(false) }
+    val nameError = remember { mutableStateOf<String?>(null) }
+    val name = remember { mutableStateOf(TextFieldValue("")) }
+    val customText = remember { mutableStateOf(TextFieldValue("")) }
+
+    FavoriteSaveDialog(
+        showSaveDialog = showSaveDialog,
+        name = name,
+        nameError = nameError,
+        onSave = {
+            CoroutineScope(Dispatchers.Main).launch {
+                favoritesViewModel.addCurrentFavoriteToFavorites(
+                    name = name.value.text,
+                )
+            }
+
+            Toast.makeText(
+                context,
+                R.string.save_sequence_toast_message,
+                Toast.LENGTH_SHORT,
+            ).show()
+
+            showCreateDialog.value = false
+        },
+    )
 
     if (showCreateDialog.value) {
         AlertDialog(
@@ -83,6 +125,8 @@ fun FavoriteDialogMaker(
                                 )
                             }
 
+                            FavoriteMakerTextInput(customText, favoritesViewModel)
+
                             Row(
                                 verticalAlignment = Alignment.CenterVertically,
                                 horizontalArrangement = Arrangement.SpaceBetween,
@@ -91,6 +135,47 @@ fun FavoriteDialogMaker(
                                         .fillMaxWidth().padding(horizontal = 12.dp)
                                         .padding(bottom = 8.dp),
                             ) {
+                                Button(
+                                    onClick = {
+                                        favoritesViewModel
+                                            .appendToCurrentFavoriteEmojiCodes(
+                                                customText.value.text,
+                                            )
+                                        customText.value = TextFieldValue("")
+                                        focusManager.clearFocus()
+                                    },
+                                    modifier = Modifier.padding(end = 8.dp),
+                                    colors =
+                                        ButtonDefaults.buttonColors(
+                                            containerColor =
+                                                MaterialTheme.colorScheme.tertiary,
+                                        ),
+                                ) {
+                                    Row {
+                                        Icon(
+                                            imageVector = Icons.Default.Add,
+                                            contentDescription =
+                                                stringResource(
+                                                    R.string.add_text_button_description,
+                                                ),
+                                            tint = MaterialTheme.colorScheme.onTertiary,
+                                        )
+
+                                        Icon(
+                                            imageVector = Icons.Default.EditNote,
+                                            contentDescription =
+                                                stringResource(
+                                                    R.string.add_text_button_description,
+                                                ),
+                                            tint = MaterialTheme.colorScheme.onTertiary,
+                                            modifier =
+                                                Modifier.padding(
+                                                    start = 3.dp,
+                                                ),
+                                        )
+                                    }
+                                }
+
                                 Button(
                                     onClick = {
                                         favoritesViewModel
@@ -118,6 +203,7 @@ fun FavoriteDialogMaker(
                             }
                         }
                     }
+
                     Card(
                         modifier =
                             Modifier
@@ -134,7 +220,7 @@ fun FavoriteDialogMaker(
                                 Text(
                                     text = stringResource(R.string.no_search),
                                     style = MaterialTheme.typography.bodyMedium,
-                                    modifier = Modifier.padding(8.dp),
+                                    modifier = Modifier.padding(10.dp),
                                 )
                             }
                         } else {
@@ -161,7 +247,7 @@ fun FavoriteDialogMaker(
             },
             confirmButton = {
                 Button(onClick = {
-                    showCreateDialog.value = false
+                    showSaveDialog.value = true
                 }) {
                     Text(stringResource(R.string.save))
                 }
@@ -169,6 +255,7 @@ fun FavoriteDialogMaker(
             dismissButton = {
                 Button(
                     onClick = {
+                        favoritesViewModel.clearCurrentFavorite()
                         showCreateDialog.value = false
                     },
                     colors =
